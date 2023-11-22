a4968fc13cd914c4a5d69e13fb82fadc3b44c67d
bpasco <benjamin.pasco@hotmail.fr>
2023-11-22 22:13:20 +0100

    feat: add query provider to add

--- packages/frontend/app/layout.tsx
+++ packages/frontend/app/layout.tsx
@@ -2,7 +2,9 @@
 
 import './global.css';
 import { useState } from 'react';
-import Mode from '../contexts/Mode';
+import Mode from '@/providers/Mode';
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from '@/providers/QueryClient';
 
 export default function RootLayout({
   children,
@@ -13,9 +15,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={mode}>
-        <Mode.Provider value={{ mode, setMode: (newMode) => setMode(newMode) }}>
-          {children}
-        </Mode.Provider>
+        <QueryClientProvider client={queryClient}>
+          <Mode.Provider
+            value={{ mode, setMode: (newMode) => setMode(newMode) }}
+          >
+            {children}
+          </Mode.Provider>
+        </QueryClientProvider>
       </body>
     </html>
   );
